e947dc07f381c793ed32f6651525bf2a402fc2ce
Hector-Amaya <111697789+Hector-Amaya@users.noreply.github.com>
2024-04-28 19:29:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
+import statistics
 import seaborn as sns
 from statsmodels.tsa.seasonal import seasonal_decompose
 
